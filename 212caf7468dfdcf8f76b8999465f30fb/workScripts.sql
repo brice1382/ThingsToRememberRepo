7533218674184f321cbd651792469b6505f3e57a
brice1382 <brice721@hotmail.com>
2017-07-01 17:11:29 -0400

    Delete workScripts.sql

--- 212caf7468dfdcf8f76b8999465f30fb/workScripts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[Deployment]
-(
-	[DeployId] INT        IDENTITY (1, 1) NOT NULL,
-	[Timeframe] NVARCHAR (50) NOT NULL,
-	[Location] NVARCHAR (50) NOT NULL,
-	PRIMARY KEY CLUSTERED ([DeployId] ASC)
-)
